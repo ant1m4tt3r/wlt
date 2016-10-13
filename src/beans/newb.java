5087b9aec762ef68a89ba97365ad4357c273079e
Hugo Azevedo <hugo.azd1@gmail.com>
2016-10-13 10:07:04 -0300

    commit feio

--- src/beans/newb.java
+++ src/beans/newb.java
@@ -5,6 +5,11 @@ public class newb {
   public newb() {
     // TODO Auto-generated constructor stub
     System.out.println("555");
+    System.out.println("tchau");
+  }
+  
+  public void nada() {
+    System.out.println("nada");
   }
   
   
